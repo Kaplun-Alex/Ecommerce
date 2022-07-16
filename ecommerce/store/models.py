c85b23cb560c7268b7f9a4af9afd901224b527b4
Alex Kaplun <77889471+Kaplun-Alex@users.noreply.github.com>
2022-07-16 10:01:55 +0000

    Working width models

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -1,3 +1,46 @@
+from email.mime import image
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Customer(models.Model):
+    user =  models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=254, null=True)
+    email = models.CharField(max_length=254)
+    
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    customer =
+    date_ordered =
+    complete = 
+    transaction_id =
+    
+
+class ShippingAdress(models.Model):
+    customer = 
+    order = 
+    adress =
+    city =
+    state =
+    country =
+    zipcode = 
+    date_added =
+
+    
+class OrderItem(models.Model):
+    product = 
+    order =
+    qantyty =
+    date_added =
+    
+
+class Product(models.Model):
+
+    name = 
+    price = 
+    digital = 
+    image =
+    
+
