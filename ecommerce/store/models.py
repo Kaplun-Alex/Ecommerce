d9d415d23f86f08d2ef9290ceabf4e141f5cd2e9
Alex Kaplun <77889471+Kaplun-Alex@users.noreply.github.com>
2022-07-20 09:30:20 +0000

    Add stat img

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -1,3 +1,4 @@
+from tokenize import blank_re
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -20,7 +21,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255, null=True)
     price = models.FloatField()
     digital = models.BooleanField(default=False, null=True, blank=True)
-    #image
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
