5a4a7beca5ac241361a602805f5cac9a5575dc01
Alex Kaplun <77889471+Kaplun-Alex@users.noreply.github.com>
2022-09-06 09:49:27 +0000

    Renev to codespace

--- store/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Customer, Product, Order, OrderItem, ShippingAdress
-
-
-admin.site.register(Customer)
-admin.site.register(Product)
-admin.site.register(Order)
-admin.site.register(OrderItem)
-admin.site.register(ShippingAdress)
